68a68e8be08fc92f0ca474e2e8c9d93b7df7fe13
Franky Coding <fran-15-16@hotmail.com>
2019-05-21 12:31:48 +0100

    Fix and Comment How do you use

--- lib/BubbleSort.dart
+++ lib/BubbleSort.dart
@@ -2,25 +2,35 @@ class BubbleSort {
 
   void bSort(List list){
 
+    // Este seria el arreglo o lista de elementos que le asignaremos
     int n = list.length;
-    int i, step;
 
-    // Metodo Burbuja
-    // El primer for son las vueltas que daremos.
-    for(step = 0; step < n ; step++){
+
+    int i, j;
+
+    // Bubble Method // Metodo Burbuja
+    // The first for is for the number of times the array travels.
+    // El primer for son las vueltas que necesitaremos dar para ordenar el array.
+    for(j = 0; j < n  ; j++){
+      print("Route: $j" );
       // El segundo es para odenar este arreglo
-      // Prueba a cambiar el < por > y veras como se desordena
-      for(i = 0; i < n - step - 1; i++){
-        // Condicional si numeroActual > numeroSiguiente.
+      // The Second is for organized the order of numbers
+      for(i = 0; i < n - j - 1; i++){
+        // Condicional si numeroActual > numeroSiguiente cambiamos.
         // Para invertir el orden solo es necesario cambiar el > por <
+        // Conditional if actualNumber > nextNumber we change
+        // if you need to change order, only change operator > for <
         if(list[i] > list[i + 1]){
+          print("Numero Actual: ${list[i]} > Numero Siguiente: ${list[i + 1]}");
           swap(list,i);
         }
       }
     }
+
     // Mostrando el arreglo ordenado en forma creciente
+    // Print the Result
     print("\n Arreglo ordenado de forma creciente: ");
-    for(i = 0; i < step ; i++){
+    for(i = 0; i < j ; i++){
       print("${list[i]}");
     }
 
